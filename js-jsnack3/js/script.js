68b0e056a1c1a108e6f401d0d7a330e3729fe070
Maria Stachie <mary.stachie@gmail.com>
2022-03-22 18:56:45 +0100

    snack 3 done

--- js-jsnack3/js/script.js
+++ js-jsnack3/js/script.js
@@ -2,3 +2,14 @@ console.log('OK JS :) ');
 
 // scrivi una f.ne che accetti una stringa come argomento e la ritorni girata
 
+function reverse(wordInput) {
+    // la f.ne accetta una stringa come argomento
+    let reversedWord = "";
+    for (let i = wordInput.length - 1; i >= 0; i--) {
+        reversedWord += wordInput[i];
+    }
+    return reversedWord;
+}
+
+let prova = reverse("yraM");
+console.log(prova);
